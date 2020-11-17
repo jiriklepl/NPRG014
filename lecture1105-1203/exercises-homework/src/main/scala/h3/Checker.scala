d97d32596b74f64e4428fd847ad3c7a1696e19ab
jiriklepl <jiriklepl@seznam.cz>
2020-11-17 03:03:24 +0100

    hw3

--- lecture1105-1203/exercises-homework/src/main/scala/h3/Checker.scala
+++ lecture1105-1203/exercises-homework/src/main/scala/h3/Checker.scala
@@ -1,5 +1,3 @@
-/* Uncomment this to finalize the code
-
 package h3
 import scala.collection.mutable.MutableList
 
@@ -12,37 +10,42 @@ class Property(val name: String, val func: () => Boolean)
 
 class Monitor[T] {
 	val properties = MutableList.empty[Property]
-	
-	def property /* Add declaration here */ {
+
+	def property(propName: String)(formula: => Boolean) = {
 		properties += new Property(propName, formula _)
 	}
-	
+
 	var eventsToBeProcessed = List[T]()
-	
+
 	def check(events: List[T]) {
 		for (prop <- properties) {
 			eventsToBeProcessed = events
-		
+
 			val result = prop.func()
-			
+
 			println("Property \"" + prop.name + "\" ... " + (if (result) "OK" else "FAILED"))
 		}
 	}
 
 	def require(func: PartialFunction[T, Boolean]): Boolean = {
-		/* Add body here
-		 * 
-		 * to know whether a partial function is defined for a given event,
-		 * use func.isDefinedAt(event).
-		 */		
+		eventsToBeProcessed match {
+			case event :: events =>
+				if (func.isDefinedAt(event)) {
+					eventsToBeProcessed = events
+					func(event)
+				} else {
+					true
+				}
+			case Nil =>
+				throw new NoSuchElementException()
+		}
 	}
-	
 }
 
 class MyMonitor extends Monitor[Event] {
 	property("The first command should succeed or fail before it is received again") {
 		require {
-			case Command(c) => 
+			case Command(c) =>
 				require {
 					case Succeed(`c`) => true
 					case Fail(`c`) => true
@@ -53,13 +56,13 @@ class MyMonitor extends Monitor[Event] {
 
 	property("The first command should not get two results") {
 		require {
-			case Succeed(c) => 
+			case Succeed(c) =>
 				require {
 					case Succeed(`c`) => false
 					case Fail(`c`) => false
 					case Command(`c`) => true
 				}
-			case Fail(c) => 
+			case Fail(c) =>
 				require {
 					case Succeed(`c`) => false
 					case Fail(`c`) => false
@@ -69,14 +72,17 @@ class MyMonitor extends Monitor[Event] {
 	}
 
 	property("The first command should succeed") {
-		/* Add a property definition here which requires that the first command does not fail.
-		 * It should yield OK with the events listed in the main method.
-		 */
+		require {
+			case Command(c) =>
+				require {
+					case Succeed(`c`) => true
+					case Fail(`c`) => false
+				}
+		}
 	}
 }
 
 object Checker {
-
 	def main(args: Array[String]) {
 		val events = List(
 			Command("take_picture"),
@@ -84,11 +90,8 @@ object Checker {
 			Succeed("take_picture"),
 			Fail("take_picture")
 		)
-		
+
 		val monitor = new MyMonitor
 		monitor.check(events)
 	}
-
 }
-
-*/
